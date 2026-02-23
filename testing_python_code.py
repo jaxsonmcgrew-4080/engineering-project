bb03d7a3375ddde86e611aaac95e0ca9d49d9b18
jaxsonmcgrew-4080 <jaxson.mcgrew@desaleshs.com>
2026-02-23 17:39:20 +0000

    fixed the calculator and added another error handler

--- testing_python_code.py
+++ testing_python_code.py
@@ -4,27 +4,31 @@ while True:
     coq = input ("1. Game 2. Calc 3. Quit : ")
     if coq == "1":
         break
-    else:        print ("Invalid input, try again")
-    if coq == "2":
-        quit()
-    elif coq == "3":
+    elif coq == "2":
+        print ("Welcome to the calculator! You can add, subtract, multiply, and divide whole numbers only")
         
-        x = int(input ("Enter your first number here WHOLE NUMBERS ONLY!!! : "))
-        y = input ("Enter one of the following to add '+' to mult '*' to divide '/' to subtract '-' : ")
-        z = int(input ("Enter your second number here WHOLE NUMBERS ONLY!!! : "))
-        if y == "+":
+    elif coq == "3":
+
+        quit()
+    elif coq != "1" and coq != "2" and coq != "3":
+        print ("Invalid input, try again")
+        continue    
+    x = int(input ("Enter your first number here WHOLE NUMBERS ONLY!!! : "))
+    y = input ("Enter one of the following to add '+' to mult '*' to divide '/' to subtract '-' : ")
+    z = int(input ("Enter your second number here WHOLE NUMBERS ONLY!!! : "))
+    if y == "+":
             result1= x + z
             print(result1)
             continue
-        elif y == "-":
+    elif y == "-":
             result2= x - z 
             print(result2)
             continue
-        elif y == "/":
+    elif y == "/":
             result3= x / z 
             print(result3)
             continue
-        elif y == "*":
+    elif y == "*":
             result4= x * z 
             print(result4)
             continue
@@ -37,6 +41,9 @@ while True:
      elif rng == "1":
         rn = (random.randint(1,25))
         grn = input ("Enter your guess 1-25 or enter 'Quit' to quit : ")
+        if grn != "Quit" and (int(grn) < 1 or int(grn) > 25):
+            print ("Invalid input, try again")
+            continue
      if grn == "Quit":
          quit()
      elif int(grn) == rn:
