cfc77e9c347fca363ae74182e0a4c108959db7eb
jaxsonmcgrew-4080 <jaxson.mcgrew@desaleshs.com>
2026-02-24 18:11:05 +0000

    added more error handlers

--- testing_python_code.py
+++ testing_python_code.py
@@ -36,12 +36,15 @@ while True:
 print ("Welcome to the dice rolling game! You will be rolling a 25 sided die and guessing the number. If you guess the number you win if you dont you lose")
 while True:
      rng = input ("Enter '1' to roll the dice or enter '2' to quit : ")
+     if rng != "1" and rng != "2":
+         print ("Invalid input, try again")
+         continue
      if rng == "2":
          quit()
      elif rng == "1":
         rn = (random.randint(1,25))
         grn = input ("Enter your guess 1-25 or enter 'Quit' to quit : ")
-        if grn != "Quit" and (int(grn) < 1 or int(grn) > 25):
+        if grn != "Quit" and (grn != "1" and grn != "2" and grn != "3" and grn != "4" and grn != "5" and grn != "6" and grn != "7" and grn != "8" and grn != "9" and grn != "10" and grn != "11" and grn != "12" and grn != "13" and grn != "14" and grn != "15" and grn != "16" and grn != "17" and grn != "18" and grn != "19" and grn != "20" and grn != "21" and grn != "22" and grn != "23" and grn != "24" and grn != "25"):
             print ("Invalid input, try again")
             continue
      if grn == "Quit":
